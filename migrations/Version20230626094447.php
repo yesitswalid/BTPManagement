68f9d298c69adfd43be6033c9b1274488559346b
yesitswalid <kerduoker@gmail.com>
2023-06-26 11:53:08 +0200

    Updated command from readme.md

--- migrations/Version20230626094447.php
+++ migrations/Version20230626094447.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230624134649 extends AbstractMigration
+final class Version20230626094447 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,13 +21,19 @@ final class Version20230624134649 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE chantier (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, start_date DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pointage (id INT AUTO_INCREMENT NOT NULL, utilisateur_id INT NOT NULL, chantier_id INT NOT NULL, date DATE NOT NULL, duree INT NOT NULL, INDEX IDX_7591B20FB88E14F (utilisateur_id), INDEX IDX_7591B20D0C0049D (chantier_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, matricule VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE pointage ADD CONSTRAINT FK_7591B20FB88E14F FOREIGN KEY (utilisateur_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE pointage ADD CONSTRAINT FK_7591B20D0C0049D FOREIGN KEY (chantier_id) REFERENCES chantier (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pointage DROP FOREIGN KEY FK_7591B20FB88E14F');
+        $this->addSql('ALTER TABLE pointage DROP FOREIGN KEY FK_7591B20D0C0049D');
         $this->addSql('DROP TABLE chantier');
+        $this->addSql('DROP TABLE pointage');
         $this->addSql('DROP TABLE user');
     }
 }
